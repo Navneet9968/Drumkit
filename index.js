c1e0f7fb157b2c1743b0bbc46d6fc0d4a4990b9b
Navneet9968 <navneetkumar9810@gmail.com>
2023-04-01 19:09:39 +0530

    uploading

---
+++ index.js
@@ -0,0 +1,73 @@
+
+
+//for mouse clicks
+for(var i = 0; i <document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        
+        var alpha=this.innerHTML;
+        makeSound(alpha);
+        buttonAnimation(alpha);
+    }); 
+}
+
+// for keyboard press events
+
+document.addEventListener("keypress", function(event){
+    // console.log(event.key);
+
+    var keystroke=event.key;
+    makeSound(keystroke);
+    buttonAnimation(keystroke);
+});
+
+//function for making sound
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        
+    
+        default:
+            console.log(alpha);
+            break;
+    }
+
+}
+
+//for glowing effects
+
+function buttonAnimation(key) {
+    var activeButton = document.querySelector("."+key);
+    console.log(activeButton);
+    activeButton.classList.add("pressed");
+
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
